beb77f704856d51928ba2409f127490dc4b60745
tdev6hk <jfalca@mtb.com>
2022-06-09 09:43:48 -0400

    Added routing between the points on the map

--- main.js
+++ main.js
@@ -3,24 +3,36 @@ import TileLayer from 'ol/layer/Tile';
 import VectorLayer from 'ol/layer/Vector';
 import {Feature, Map, View} from 'ol';
 import {Circle as CircleStyle, Fill, Stroke, Style, Text} from 'ol/style';
+import Polyline from 'ol/format/Polyline';
 import {Vector as VectorSource} from 'ol/source';
 import {fromLonLat} from 'ol/proj';
 import Point from 'ol/geom/Point';
 
+/**
+ * Tech Connect Project # 1
+ * Restaurant Map Pathfinding
+ */
+
 //Vector Source containing features on map
 const source = new VectorSource();
+const coordinatesList = []
 
 //Basic client to host JSON data
 const client = new XMLHttpRequest();
 client.open('GET', './data/test_data.json');
 
+
+// Creates features from JSON data
+// Creates routes IN ORDER from JSON input
 client.onload = function () {
     const json = JSON.parse(client.responseText);
     const features = [];
 
     for (let location of json){
+      let coords = [location["lon"],location["lat"]]
+      coordinatesList.push(coords)
       let feature = new Feature({
-        geometry: new Point(fromLonLat([location["lon"],location["lat"]])),
+        geometry: new Point(fromLonLat(coords)),
       })
 
       //Styling configuration and text
@@ -51,9 +63,10 @@ client.onload = function () {
       feature.setStyle(style)
       features.push(feature)
     }
-
+    console.log(features)
     //Add the features to the Vector Source
     source.addFeatures(features);
+    renderRoutes(coordinatesList)
   };
   client.send();
 
@@ -81,9 +94,55 @@ const map = new Map({
   view: view
 });
 
-// This is so the map refreshes itself
+// To allow the map to refresh itself
 function animate() {
   map.render();
   window.requestAnimationFrame(animate);
 }
 animate();
+
+// Renders routes between a list of coordinates in order
+async function renderRoutes(pointsList){
+  for (let i = 0; i < pointsList.length; i++){
+    let loc1 = pointsList[i].toString()
+    let loc2 = pointsList[i + 1].toString()
+    renderRoute(loc1, loc2)
+    if((i + 1) == pointsList.length - 1){
+      break;
+    }
+
+  }
+}
+
+// Adds a route by creating a Polyline Feature from API call
+async function renderRoute(loc1, loc2){
+  const url_osrm_route = '//router.project-osrm.org/route/v1/driving/';
+  let res = await fetch(url_osrm_route + loc1+ ";" + loc2)
+  let json = await res.json()
+  let feat = createRouteFeature(json.routes[0].geometry)
+  source.addFeature(feat)
+}
+
+// Polyline Feature Object and Style
+function createRouteFeature(polyline){
+  var route = new Polyline({
+      factor: 1e5
+      }).readGeometry(polyline, {
+          dataProjection: 'EPSG:4326',
+          featureProjection: 'EPSG:3857'
+      });
+  
+      var feature = new Feature({
+  type: 'route',
+  geometry: route
+  });
+
+  let routeStyle = new Style({
+      stroke: new Stroke({
+      width: 6, color: [10, 10, 10, 0.7]
+      })
+  })
+
+  feature.setStyle(routeStyle);
+  return feature
+}
