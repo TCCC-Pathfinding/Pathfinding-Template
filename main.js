3a1fe97ecb961bbf79f0531e6a7be598c7009a23
tdev6hk <jfalca@mtb.com>
2022-06-13 11:36:56 -0400

    Modified routing function to work as a dumb router rather than automatic

--- main.js
+++ main.js
@@ -3,10 +3,10 @@ import TileLayer from 'ol/layer/Tile';
 import VectorLayer from 'ol/layer/Vector';
 import {Feature, Map, View} from 'ol';
 import {Circle as CircleStyle, Fill, Stroke, Style, Text} from 'ol/style';
-import Polyline from 'ol/format/Polyline';
 import {Vector as VectorSource} from 'ol/source';
 import {fromLonLat} from 'ol/proj';
 import Point from 'ol/geom/Point';
+import LineString from 'ol/geom/LineString';
 
 /**
  * Tech Connect Project # 1
@@ -16,24 +16,32 @@ import Point from 'ol/geom/Point';
 //Vector Source containing features on map
 const source = new VectorSource();
 
+
+
 //Basic client to host JSON data
 const client = new XMLHttpRequest();
-client.open('GET', './data/test_data.json');
 
 /**
  * IMPORTANT! SET THIS TO YOUR LOCATION
  */
-const city = "buffalo" //Choose "buffalo" or "wilmington" based on your location
+ const city = "buffalo" //Choose "buffalo" or "wilmington" based on your location
+ client.open('GET', './data/buffalo_graph.json');
+//  client.open('GET', './data/wilmington_graph.json');
 
 // Creates features from JSON data
 // Creates routes IN ORDER from JSON input
 client.onload = function () {
-    const json = JSON.parse(client.responseText)[city.toLowerCase()];
+    const json = JSON.parse(client.responseText)["nodes"];
+    console.log(json)
     const features = [];
     const coordinatesList = []
 
     for (let location of json){
       let coords = [location["lon"],location["lat"]]
+      let id = /[a-zA-Z]/.test(location["id"]) ? location["id"] : ""
+      if(id === ""){
+        
+      }
       coordinatesList.push(coords)
       let feature = new Feature({
         geometry: new Point(fromLonLat(coords)),
@@ -42,18 +50,18 @@ client.onload = function () {
       //Styling configuration and text
       const style = new Style({
         image: new CircleStyle({
-          radius: 4,
+          radius: 2,
           fill: new Fill({
             color: '#0084ff',
           }),
           stroke: new Stroke({
             color: 'black',
-            width: 2,
+            width: 1,
           }),
         }),
         text: new Text({
           font: '12px Calibri,sans-serif',
-          text: location["location"],
+          text: id,
           textBaseline: 'bottom',
           fill: new Fill({
             color: 'rgba(0,0,0,1)'
@@ -67,16 +75,15 @@ client.onload = function () {
       feature.setStyle(style)
       features.push(feature)
     }
-    console.log(features)
     //Add the features to the Vector Source
     source.addFeatures(features);
 
     /**
-     * TODO: Implement findShortestPath function to find the shortest path between the list of coordinates
+     * TODO: Implement findShortestPath function to find the shortest path between the restaurants in list of coordinates
      */
     let shortestPath = findShortestPath(coordinatesList)
 
-    renderRoutes(shortestPath)
+    // renderRoutes(shortestPath)
   };
   client.send();
 
@@ -137,46 +144,16 @@ animate();
 
 // Renders routes between a list of coordinates in order
 async function renderRoutes(pointsList){
-  for (let i = 0; i < pointsList.length; i++){
-    let loc1 = pointsList[i].toString()
-    let loc2 = pointsList[i + 1].toString()
-    renderRoute(loc1, loc2)
-    if((i + 1) == pointsList.length - 1){
-      break;
-    }
-
-  }
-}
-
-// Adds a route by creating a Polyline Feature from API call
-async function renderRoute(loc1, loc2){
-  const url_osrm_route = '//router.project-osrm.org/route/v1/driving/';
-  let res = await fetch(url_osrm_route + loc1+ ";" + loc2)
-  let json = await res.json()
-  let feat = createRouteFeature(json.routes[0].geometry)
-  source.addFeature(feat)
-}
-
-// Polyline Feature Object and Style
-function createRouteFeature(polyline){
-  var route = new Polyline({
-      factor: 1e5
-      }).readGeometry(polyline, {
-          dataProjection: 'EPSG:4326',
-          featureProjection: 'EPSG:3857'
-      });
-  
-      var feature = new Feature({
-  type: 'route',
-  geometry: route
-  });
-
+  let line = new LineString(pointsList).transform('EPSG:4326', 'EPSG:3857')
+  var feature = new Feature(line);
   let routeStyle = new Style({
       stroke: new Stroke({
-      width: 6, color: [10, 10, 10, 0.7]
+      width: 3, 
+      color: [10, 10, 10, 0.7]
       })
   })
 
   feature.setStyle(routeStyle);
-  return feature
+  source.addFeature(feature)
+
 }
